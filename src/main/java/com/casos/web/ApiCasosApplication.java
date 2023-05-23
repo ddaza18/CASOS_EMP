6b207219f8688ef9c02532d5de5314f3b6afc40f
Daniel <ddaza681@gmail.com>
2023-05-22 20:46:00 -0500

    Se hace el desarrollo del BOT con envio de respuesta automatica hacia el canal a un ID de usuario especifico(Telegram)

--- src/main/java/com/casos/web/ApiCasosApplication.java
+++ src/main/java/com/casos/web/ApiCasosApplication.java
@@ -1,13 +1,39 @@
 package com.casos.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import com.casos.web.components.TelegramBotAPI;
 
 @SpringBootApplication
 public class ApiCasosApplication {
+public static final Logger LOG = LoggerFactory.getLogger(ApiCasosApplication.class);
+
+	
+	private final TelegramBotAPI telegramBotAPI;
+	
+	@Autowired
+	public ApiCasosApplication(TelegramBotAPI telegramBotAPI) {
+		this.telegramBotAPI = telegramBotAPI;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCasosApplication.class, args);
+		
+		//Se tiene que registar el BOT para su funcionamiento
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new TelegramBotAPI());
+			LOG.info("Conectando con el BOT de Telegram -> STATUS 200|OK");
+		}catch(TelegramApiException e) {
+			LOG.error("Fallo en Registro del BOT a la aplicacion.", e.getMessage());
+		}
 	}
 
 }
---
+++ src/main/java/com/casos/web/components/TelegramBotAPI.java
@@ -0,0 +1,62 @@
+package com.casos.web.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBotAPI extends TelegramLongPollingBot{
+public static final Logger LOG = LoggerFactory.getLogger(TelegramBotAPI.class);
+	
+	@Override
+	public void onUpdateReceived(Update update) {
+		
+		String mensaje = update.getMessage().getText();
+		LOG.info("Escribieron en el BOT: " + mensaje);
+		
+		//Obtiene el id del usuario y lo parsea
+		final String chatId = String.valueOf(update.getMessage().getChatId());
+		
+		//Crea un objeto del mensaje
+		SendMessage message = new SendMessage();
+		message.setChatId(chatId);
+		message.setText("Gracias por escribirnos por el chat de CASOS EMP");
+		try {
+			//Envia el mensaje al chat Telegram
+			execute(message);
+			LOG.info("Respuesta enviada al BOT de telegram.");
+			/*if(adminTelegramRepository != null) {
+				
+			mensajesTelegram.setFechaenvio(new Date());
+			mensajesTelegram.setMensaje(mensaje);
+			LOG.info("Guardando Mensaje recibido a BD");
+			adminTelegramRepository.save(mensajesTelegram);
+			}
+			else {
+				LOG.error("Error al guardar el mensaje de Telegram.");
+			}*/
+		}catch(TelegramApiException e){
+			LOG.error("Ocurrio un error en el envio de la respuesta al BOT de telegram", e.getMessage());
+		}
+		
+	}
+	
+	/**
+	 * Token de acceso para el BOT y nombre
+	 */
+	@Override
+	public String getBotUsername() {
+		String nameBot = "pruebasemp_bot";
+		return nameBot;
+	}
+
+	@Override
+	public String getBotToken() {
+		String tokenAcceso = "6270890199:AAESq1AUIjcFdNmakSksch0BYPKlN1sd_3k";
+		return tokenAcceso;
+	}
+}
